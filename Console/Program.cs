230a046abbd2b0b646a4b2d239b5cf709ba77d3a
Joel <joel@horowitz.com>
2015-07-06 22:46:00 -0400

    Cleanup and new ignores

--- Console/Program.cs
+++ Console/Program.cs
@@ -11,30 +11,27 @@ namespace Console
 {
     class Program
     {
-        static void Main()
+        private static Tuple<IEnumerable<SalesRecord>, IEnumerable<string>> GetRecords()
         {
-            Directory.SetCurrentDirectory(@"..\..\..\HTMLDoc\assets");
-
-            var jsdoc = new HtmlDocDataTableComponent();
-            //	var headers = new List<string>() { "AAA", "BBB", "CCC" };
-            //	var table = new List<List<object>>() {
-            //		new List<object>() { "XXX", 123, 5.4 },
-            //		new List<object>() { "YuY", 54, 63 },
-            //		new List<object>() { "YuY", DateTime.Now, 63 }
-            //	};
-
             var csvFile = @"sample data\sampledata.csv";
             var reader = new StreamReader(csvFile);
 
             var csv = new CsvHelper.CsvReader(reader);
             csv.Configuration.AutoMap<SalesRecord>();
             var headers = typeof(SalesRecord).GetProperties().Select(p => p.Name);
-            var records = csv.GetRecords<SalesRecord>().ToList();
+            var records = csv.GetRecords<SalesRecord>();
+            return Tuple.Create(records, headers);
+        }
+
+        static void Main()
+        {
+            Directory.SetCurrentDirectory(@"..\..\..\HTMLDoc\assets");
+
 
             var htmldoc = new HTMLDoc.HTMLDoc();
             htmldoc.AddCSSLink("lib/3rdParty/bootstrap/css/bootstrap.css");
             htmldoc.AddCSSLink("lib/3rdParty/bootstrap/css/bootstrap-responsive.css");
-            //htmldoc.AddJSLink("bootstrap/js/jquery.js");
+            htmldoc.AddJSLink("lib/3rdParty/bootstrap/js/jquery.js");
             htmldoc.AddJSLink("lib/3rdParty/bootstrap/js/bootstrap.js");
             htmldoc.AddJSLink("lib/3rdParty/angular.min.js");
             
@@ -43,6 +40,10 @@ namespace Console
             htmldoc.AddJSLink("lib/htmldoctableDirective.js");
             htmldoc.StartBody();
             htmldoc.AddToBody("<H1>Table</H1>");
+
+            var data = GetRecords();
+            var records = data.Item1;
+            var headers = data.Item2;
             htmldoc.AddTable<SalesRecord>(headers, records.Take(5));
 
             var persons = new List<PersonRecord>() {
